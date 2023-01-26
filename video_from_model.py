4d95f9d67fd5d454c34c261e8f6c91bc158a8fab
Gabriel Nascarella <ielnascarella@hotmail.com>
2023-01-26 19:33:03 -0300

    try new input

--- video_from_model.py
+++ video_from_model.py
@@ -180,13 +180,13 @@ if __name__ == '__main__':
                 draw_gaze(x_min,y_min,bbox_width, bbox_height,frame,(pitch_predicted,yaw_predicted),color=(0,0,255))
                 cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0,255,0), 1)
 
-                angle_values = np.array((pitch_predicted,yaw_predicted))
+                angle_values = np.array(np.array((pitch_predicted,yaw_predicted)))
 
                 automl_model = pickle.load(open("model", "rb"))
 
                 print(f"inputs: {angle_values}")
 
-                prediction = automl_model.predict(angle_values)
+                prediction = automl_model.predict(angle_values)[0]
                 if prediction:
                     output_str = f"Looking at road elements"
                     color = (0, 255, 100)
